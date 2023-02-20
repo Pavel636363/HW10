338b6b02e0b4dbf23e047132260f870db59f938e
pavel <pavel_sgau@rambler.ru>
2023-02-20 22:10:52 +0500

    initial commit

---
+++ utils.py
@@ -0,0 +1,18 @@
+import json
+from pprint import pprint
+import utils
+
+def load_candidates():
+    with open('candidates.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        candidates = {}
+        for i in data:
+            candidates[i['id']] = i
+        return candidates
+
+load_candidates()
+
+
+
+
+
